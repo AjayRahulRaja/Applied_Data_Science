760da2312a5c9d6f120c978fa0530f6569a7922b
Ajay Rahul <21rahulajay@gmail.com>
2023-03-14 22:51:43 +0000

    Second commit

--- Assignemnt_02/Main/driverProgram.py
+++ Assignemnt_02/Main/driverProgram.py
@@ -1,17 +1,36 @@
 import pandas as pd
 
 df = pd.read_excel("C://Users/Lenovo/Desktop/UK/Hertfordshire/SEM 01/Applied Data Science/viz proj 02/API_19_DS2_en_excel_v2_4903056.xls", header=3)
-print(df.head(2))
 
-df=df.drop(['Country Code', 'Indicator Code'], axis=1)
-df.set_index(['Country Name', 'Indicator Name'], inplace=True)
-df.sort_index(inplace=True)                # sort index
-print(df.head(3))
+removeColumns=['Country Code', 'Indicator Code']
+indexColumns=["Country Name", "Indicator Name"]
 
-df=df.T
+def transformDf(df, removeColumns, indexColumns):
+    df.drop(removeColumns, axis=1)
+    df.set_index(indexColumns, inplace=True)
+    df.sort_index(inplace=True)
+    return df
+
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
-print(df.head(4))
+new_df=transformDf(df, removeColumns, indexColumns)
+new_df.head(2)
 
+def transformDf2(df):
+    df.fillna(0)
+    df=df.T
+    return df
 
+new_df1=transformDf2(new_df)
+new_df1.head(2)
+
+countryList=['France', 'United Kingdom', 'India', 'Japan', 'Cuba', 'Colombia', 'Iraq', 'Algeria', 'Australia']
+def fillterCountries(df):
+    d={}
+    for x in countryList:
+        d[x] = df.loc[:, x]
+    return d
+
+x=fillterCountries(new_df1)
+France, United_Kingdom, India, Japan, Cuba, Colombia, Iraq, Algeria, Australia=x['France'], x['United Kingdom'], x['India'], x['Japan'], x['Cuba'], x['Colombia'], x['Iraq'], x['Algeria'], x['Australia']
